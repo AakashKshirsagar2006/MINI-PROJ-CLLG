f726e1541f4b72f8833b744d2b2ed100a4c9e48c
Sarvagya Mishra <msarvagya2004@gmail.com>
2026-03-03 22:02:45 +0530

    more

--- server/app.js
+++ server/app.js
@@ -23,8 +23,6 @@ const app = express();
 app.use(cors({
   origin: process.env.CORS_URI,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use("/uploads", express.static("uploads", {
